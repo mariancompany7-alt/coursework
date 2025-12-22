e03764decb55052d6dd1e7aa41807a8d29d31e44
Marian0954 <mariancompany7@gmail.com>
2025-12-22 04:25:31 +0200

    Delete P directory

--- P/room.cpp
+++
@@ -1,6 +0,0 @@
-#include "room.h"
-
-Room::Room() : id(0), roomNumber(0), price(0), status("Вільний") {}
-
-Room::Room(int id, int roomNumber, const QString& type, int price, const QString& status)
-    : id(id), roomNumber(roomNumber), type(type), price(price), status(status) {}
